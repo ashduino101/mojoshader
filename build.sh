99b97e2569935c7c6f98f5ee6ee5f5b04ae4f5f3
ashduino101 <ashduino101@gmail.com>
2024-02-20 15:24:17 -0800

    oops

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+emcmake cmake .
 emcc mojoshader.c mojoshader_common.c mojoshader_opengl.c mojoshader_metal.c profiles/mojoshader_profile_arb1.c profiles/mojoshader_profile_bytecode.c profiles/mojoshader_profile_common.c profiles/mojoshader_profile_d3d.c profiles/mojoshader_profile_glsl.c profiles/mojoshader_profile_hlsl.c profiles/mojoshader_profile_metal.c  \
 mojoshader_effects.c dxdisassemble.c \
 -DSUPPORT_PROFILE_D3D=0 -DSUPPORT_PROFILE_SPIRV=0 -DSUPPORT_PROFILE_GLSPIRV=0 -DMOJOSHADER_EFFECT_SUPPORT \
